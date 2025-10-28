7afcfcb6676572ffb8311e3821873a929a59c333
jlfan <45466202@qq.com>
2025-10-28 21:13:38 +0800

    temp for timer

--- os/src/timer.rs
+++ os/src/timer.rs
@@ -20,13 +20,13 @@ pub fn get_time() -> usize {
 /// get current time in milliseconds
 #[allow(dead_code)]
 pub fn get_time_ms() -> usize {
-    time::read() * MSEC_PER_SEC / CLOCK_FREQ
+    time::read()
 }
 
 /// get current time in microseconds
 #[allow(dead_code)]
 pub fn get_time_us() -> usize {
-    time::read() * MICRO_PER_SEC / CLOCK_FREQ
+    time::read()
 }
 
 /// Set the next timer interrupt
