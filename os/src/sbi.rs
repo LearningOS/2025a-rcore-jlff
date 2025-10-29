67452e8d7d5fc5d6debc84ecd5a74d264267a8cd
jlfan <45466202@qq.com>
2025-10-29 21:12:19 +0800

    update

--- os/src/sbi.rs
+++ os/src/sbi.rs
@@ -7,8 +7,8 @@ use core::arch::asm;
 const SBI_SET_TIMER: usize = 0;
 const SBI_CONSOLE_PUTCHAR: usize = 1;
 const SBI_CONSOLE_GETCHAR: usize = 2;
-const SBI_SHUTDOWN: usize = 8;
-
+//const SBI_SHUTDOWN: usize = 8;
+const SBI_SHUTDOWN: usize = 0x53525354;
 /// general sbi call
 #[inline(always)]
 fn sbi_call(which: usize, arg0: usize, arg1: usize, arg2: usize) -> usize {
