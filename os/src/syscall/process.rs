ac0c92b50b1303802fb8f613f1a7406524dc0f25
jlfan <45466202@qq.com>
2025-10-31 14:54:43 +0800

    systrace

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,5 +1,7 @@
 //! Process management syscalls
-use crate::task::{change_program_brk, exit_current_and_run_next, suspend_current_and_run_next};
+
+use crate::{mm::{PageTable, PageTableEntry, PhysAddr, VirtAddr, VirtPageNum}, task::{change_program_brk, current_user_token, exit_current_and_run_next, suspend_current_and_run_next}};
+use crate::mm::PhysPageNum;
 
 #[repr(C)]
 #[derive(Debug)]
@@ -30,13 +32,88 @@ pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
     -1
 }
 
-/// TODO: Finish sys_trace to pass testcases
-/// HINT: You might reimplement it with virtual memory management.
-pub fn sys_trace(_trace_request: usize, _id: usize, _data: usize) -> isize {
-    trace!("kernel: sys_trace");
-    -1
+pub fn v2p(vaddr: usize) -> Option<PhysAddr> {
+    let page_table : PageTable = PageTable::from_token(current_user_token());
+    let vppn: VirtPageNum = VirtAddr::from(vaddr).floor().into();
+    let entry: Option<PageTableEntry> = page_table.translate(vppn);
+    if entry.is_none() || !entry.unwrap().is_valid() || !entry.unwrap().is_user() {
+        return None;
+    }
+    let ppn: PhysPageNum = entry.unwrap().ppn();
+    let mut phyaddr:usize = PhysAddr::from(ppn).into();
+    phyaddr += VirtAddr::from(vaddr).page_offset();
+    Some(PhysAddr::from(phyaddr))
 }
 
+/// TODO: Finish sys_trace to pass testcase
+/// HINT: You might reimplement it with virtual memory management.
+/// 引入虚存机制后，原来内核的 sys_get_time 和 sys_trace 函数实现就无效了。请你重写这两个系统调用的代码，恢复其正常功能。
+
+// 此外，由于本章我们有了地址空间作为隔离机制，所以 sys_trace 需要考虑一些额外的情况：
+
+// 在读取（trace_request 为 0）时，如果对应地址用户不可见或不可读，则返回值应为 -1（isize 格式的 -1，而非 u8）。
+
+// 在写入（trace_request 为 1）时，如果对应地址用户不可见或不可写，则返回值应为 -1（isize 格式的 -1，而非 u8）。
+
+pub fn sys_trace(trace_request: usize, id: usize, _data: usize) -> isize {
+    trace!("kernel: sys_trace");
+    let phaddr:Option<PhysAddr> = v2p(id);
+    if phaddr.is_none() {
+        return -1;
+    }
+    let addr:usize = phaddr.unwrap().into();
+    match trace_request {
+        0 => {
+            let address = addr as *const u8; 
+            unsafe {
+                let ret = *address;
+                return ret as isize;
+            }
+        }
+        1 => {
+            let address = addr as *mut u8;
+            unsafe {
+                *address = _data as u8; 
+            }
+            return 0;
+        }
+        2 => {return -1; }
+        _ => {return -1;}
+    }
+}
+
+// mmap 和 munmap 匿名映射¶
+// mmap 在 Linux 中主要用于在内存中映射文件， 本次实验简化它的功能，仅用于申请内存。
+
+// 请实现 mmap 和 munmap 系统调用，mmap 定义如下：
+
+// fn sys_mmap(start: usize, len: usize, prot: usize) -> isize
+// syscall ID：222
+
+// 申请长度为 len 字节的物理内存（不要求实际物理内存位置，可以随便找一块），将其映射到 start 开始的虚存，内存页属性为 prot
+
+// 参数：
+// start 需要映射的虚存起始地址，要求按页对齐
+
+// len 映射字节长度，可以为 0
+
+// prot：第 0 位表示是否可读，第 1 位表示是否可写，第 2 位表示是否可执行。其他位无效且必须为 0
+
+// 返回值：执行成功则返回 0，错误返回 -1
+
+// 说明：
+// 为了简单，目标虚存区间要求按页对齐，len 可直接按页向上取整，不考虑分配失败时的页回收。
+
+// 可能的错误：
+// start 没有按页大小对齐
+
+// prot & !0x7 != 0 (prot 其余位必须为0)
+
+// prot & 0x7 = 0 (这样的内存无意义)
+
+// [start, start + len) 中存在已经被映射的页
+
+// 物理内存不足
 // YOUR JOB: Implement mmap.
 pub fn sys_mmap(_start: usize, _len: usize, _port: usize) -> isize {
     trace!("kernel: sys_mmap NOT IMPLEMENTED YET!");
